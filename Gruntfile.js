7d8ed91a5ce8ed2efb1b79514e19af918a240cb6
Ellen Gummesson <ellengummesson@gmail.com>
2013-12-23 14:27:59 +0100

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+    jshint: {
+      files: [
+        'Gruntfile.js',
+        'lib/*.js',
+        'test/*.js'
+      ],
+      options: {
+      newcap: false
+      }
+    },
+
+    open: {
+      test: {
+        path: 'test/test.html'
+      }
+    },
+
+    uglify: {
+      options: {
+        report: 'gzip'
+      },
+
+      dist: {
+        src: ['lib/cliche.js'],
+        dest: 'dist/cliche.min.js'
+      }
+    }
+  });
+
+  grunt.registerTask('test', [
+    'jshint',
+    'open'
+  ]);
+
+  grunt.registerTask('dist', [
+    'test',
+    'uglify'
+  ]);
+
+  grunt.registerTask('default', [
+    'test'
+  ]);
+
+};
---
+++ test/cliche-test.js
@@ -0,0 +1,80 @@
+(function() {
+
+  'use strict';
+
+  test('Single click event', function() {
+    var one = document.querySelector('#one');
+    var click = new Event('click');
+
+    Cliche({
+      '#one' : function() {
+        one.style.color = 'red';
+      }
+    });
+
+    one.dispatchEvent(click);
+    strictEqual(one.style.color, 'red');
+  });
+
+
+  test('Single click event with target', function() {
+    var one = document.querySelector('#one');
+    var click = new Event('click');
+
+    Cliche({
+      '#one' : function(event) {
+        if (event.target.innerText == 'One') {
+          one.innerText = '1';
+        }
+      }
+    });
+
+    one.dispatchEvent(click);
+    strictEqual(one.innerText, '1');
+  });
+
+  test('Multiple click events', function() {
+    var one = document.querySelector('#one');
+    var two = document.querySelector('#two');
+    var click = new Event('click');
+
+    Cliche({
+      '#one' : function() {
+        one.style.color = 'blue';
+      },
+      '#two' : function() {
+        two.className = 'three';
+      }
+    });
+
+    one.dispatchEvent(click);
+    two.dispatchEvent(click);
+    strictEqual(one.style.color, 'blue');
+    strictEqual(two.className, 'three');
+  });
+
+  test('Multiple click events with targets', function() {
+    var one = document.querySelector('#one');
+    var two = document.querySelector('#two');
+    var click = new Event('click');
+
+    Cliche({
+      '#one' : function(event) {
+        if (event.target.id == 'one') {
+          one.innerText = 'Uno';
+        }
+      },
+      '#two' : function(event) {
+        if (event.target.id == 'two') {
+          two.innerText = 'Dos';
+        }
+      }
+    });
+
+    one.dispatchEvent(click);
+    two.dispatchEvent(click);
+    strictEqual(one.innerText, 'Uno');
+    strictEqual(two.innerText, 'Dos');
+  });
+
+})();
